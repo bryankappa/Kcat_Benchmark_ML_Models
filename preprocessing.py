b4c356f6630fe67247636e35d7dee5159ab8096c
Your Name <bryancheong51@gmail.com>
2023-08-25 17:13:13 -0500

    nice

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,6 @@ import numpy as np
 
 def preprocessing(csv_file_path):
     data = pd.read_csv(csv_file_path)
-    data.columns = ["EC_number", "Species", "Structure", "Compound", "Amino", "Kcat", "unit"]
+    data.columns = ["EC_number", "Species", "Compound", "Compound_name", "Amino_encoding", "Kcat", "unit"]
     data = pd.DataFrame(data)
     return data

951436962a175034c26a62bdc233541a13748cf1
Your Name <bryancheong51@gmail.com>
2023-09-27 16:10:02 -0500

    src added some comments

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ y = data_pre['Kcat'].values
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+#run the model that is available: CustomRandomForestRegressor, customSVMregressor, and customXGBregressor
 model = customSVMregressor(verbose=2)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)

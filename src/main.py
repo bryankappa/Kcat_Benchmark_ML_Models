5e94d4f56f60c3f1f3c83f113a6efc46e3163669
Your Name <bryancheong51@gmail.com>
2023-09-28 23:40:58 -0500

    fixed

--- src/main.py
+++ src/main.py
@@ -6,18 +6,26 @@ from benchmarks.xgboost_benchmark import *
 from benchmarks.svm_benchmark import *
 from utils.performance_metrics import *
 
-preprocessor, amino_encoded = load_data()
+preprocessor, jazzy_data= load_data()
 data = preprocessor.assign_column()
-data_pre = preprocessor.apply_log_transform(data)
+data_pre = preprocessor.apply_log_transform(jazzy_data)
+jazz_encoded = pd.read_csv(r'C:\Users\Gilbert\Documents\BCB_Research\Kcat_Benchmark_ML_Models\Data\jazz_encoded.csv')
 
-X = amino_encoded
+data_pre = pd.DataFrame(data_pre)
+jazz_encoded  = pd.DataFrame(jazz_encoded)
+final_df = pd.concat([jazz_encoded, data_pre], axis=1)
+# final_df = final_df.drop('Unnamed: 0', axis=1)
+# print(final_df.head())
+final_df.drop('Kcat', axis=1)
+
+X = final_df.drop('Kcat', axis=1)
 y = data_pre['Kcat'].values
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #run the model that is available: CustomRandomForestRegressor, customSVMregressor, and customXGBregressor
-model = customSVMregressor(verbose=2)
+model = CustomRandomForestRegressor(verbose=2)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 

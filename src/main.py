2d873f71d05c48924a0ee50c4a6b1a64bfaeb413
Your Name <bryancheong51@gmail.com>
2023-10-17 02:04:27 -0500

    fixed

--- src/main.py
+++ src/main.py
@@ -2,31 +2,38 @@ import sys
 sys.path.append('C:/Users/Gilbert/Documents/BCB_Research/Kcat_Benchmark_ML_Models/')
 from utils.data_preprocessor import *
 from benchmarks.randomforest_benchmark import *
-from benchmarks.xgboost_benchmark import *
+from benchmarks.boosting_benchmark import *
 from benchmarks.svm_benchmark import *
 from utils.performance_metrics import *
 
-preprocessor, jazzy_data= load_data()
-data = preprocessor.assign_column()
-data_pre = preprocessor.apply_log_transform(jazzy_data)
-jazz_encoded = pd.read_csv(r'C:\Users\Gilbert\Documents\BCB_Research\Kcat_Benchmark_ML_Models\Data\jazz_encoded.csv')
+preprocessor = load_data() # loads main data
+data = preprocessor.assign_column() # assings columns to main data
+amino_pca = PCA(r'C:\Users\Gilbert\Documents\BCB_Research\Kcat_Benchmark_ML_Models\Data\encoded_amino.csv', n_components=433)
+amino_pca = amino_pca.apply_pca()
 
-data_pre = pd.DataFrame(data_pre)
-jazz_encoded  = pd.DataFrame(jazz_encoded)
-final_df = pd.concat([jazz_encoded, data_pre], axis=1)
-# final_df = final_df.drop('Unnamed: 0', axis=1)
-# print(final_df.head())
-final_df.drop('Kcat', axis=1)
-
-X = final_df.drop('Kcat', axis=1)
-y = data_pre['Kcat'].values
+X = amino_pca
+y = np.log10(data['Kcat'])
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #run the model that is available: CustomRandomForestRegressor, customSVMregressor, and customXGBregressor
-model = CustomRandomForestRegressor(verbose=2)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+# model = customXGBregressor(verbose=2)
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
 
-evaluate_model(y_test, y_pred)
+# evaluate_model(y_test, y_pred)
+
+rf = RandomForestRegressor()
+
+# Initialize the tuner class
+tuner = RandomForestTuner(rf)
+
+# Fit the data and search for the best hyperparameters
+best_params = tuner.fit(X_train, y_train)
+
+# Retrieve the best parameters
+print("Best parameters found: ", best_params)
+
+# Or you could use the method to get the best parameters
+print("Best parameters found: ", tuner.get_best_params())

bd462f9b3a997d11048ea15bb14225b89c1b677f
Your Name <bryancheong51@gmail.com>
2023-10-20 15:07:23 -0400

    lol

--- benchmarks/svm_benchmark.py
+++ benchmarks/svm_benchmark.py
@@ -1,6 +1,8 @@
 from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+import numpy as np
 
 class customSVMregressor:
     def __init__(self, **kwargs):
@@ -30,6 +32,22 @@ class customSVMregressor:
     def set_params(self, **params):
         """Set new parameters for the regressor."""
         self.regressor.set_params(**params)
+
+class SVMtuner:
+# create a isntance class
+    def __init__(self, svr):
+        self.svr = svr
+        self.best_params_ = None #storing variable to update the best_params_ when you add more tuning.
+
+    def fit(self, X, y):
+        # now set the best_params_
+
+        C = [0.1, 1, 10]
+        epsilon = [0.01, 0.1, 1]
+        kernel = ['sigmoid', 'linear', 'rbf', 'poly']
+
+
+
 
 
 

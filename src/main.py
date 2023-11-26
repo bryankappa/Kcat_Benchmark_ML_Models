5c612f2e9aea3444e9a171a2588e8b260fdfba44
Your Name <bryancheong51@gmail.com>
2023-11-25 23:24:57 -0500

    added src

--- src/main.py
+++ src/main.py
@@ -8,17 +8,19 @@ from utils.performance_metrics import *
 
 preprocessor = load_data() # loads main data
 data = preprocessor.assign_column() # assings columns to main data
-amino_pca = PCA(r'C:\Users\Gilbert\Documents\BCB_Research\Kcat_Benchmark_ML_Models\Data\encoded_amino.csv', n_components=433)
-amino_pca = amino_pca.apply_pca()
-
-X = amino_pca
+# aminoencoding = PCA("C:\\Users\Gilbert\Documents\BCB_Research\Kcat_Benchmark_ML_Models\Data\encoded_amino.csv", n_components=433)
+# amino_encoding = PCA.apply_pca(aminoencoding)
+X = data
 y = np.log10(data['Kcat'])
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+print(X_train.shape)
+print(y_train.shape)
+
 #run the model that is available: CustomRandomForestRegressor, customSVMregressor, and customXGBregressor
-model = customSVMregressor(kernel = 'rbf', epsilon = 1, C = 10) # you can add the specific tuning parameters needed.
+model = CustomRandomForestRegressor(verbose=2) # you can add the specific tuning parameters needed.
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print(evaluate_model(y_test, y_pred))
@@ -27,3 +29,6 @@ print(evaluate_model(y_test, y_pred))
 # tuner = SVRTuner(model)
 # best_params = tuner.fit(X_train, y_train)
 # print(f"Best parameters: {best_params}")
+
+
+
